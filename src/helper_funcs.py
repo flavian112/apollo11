00ebf42501d8dcb00722b65604166be49f809e00
Fe1bs <feibs.god@gmail.com>
2018-05-30 14:40:46 +0200

    Update helper_funcs.py
    Integrationsmethode 2. Grades

--- src/helper_funcs.py
+++ src/helper_funcs.py
@@ -1,30 +1,30 @@
 #!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
 
-def numerical_integrate(explicit_diff, x_0, t_0, dt, steps=1):
-	return runge_kutta(explicit_diff, x_0, t_0, dt, setps=steps)
+import numpy as np
 
-def runge_kutta(explicit_diff, x_0, t_0, dt, steps):
-	x_points = [x_0]
-    	y_points = [y_0]
-    	for i in range(steps):
-        	x = x_points[-1]
-        	yp = y_points[-1]
-        
-        	k1 = explicit_diff(x, yp)
-        	yd1 = k1 * (dt/2) + yp
-        
-        	k2 = explicit_diff(x+(dt/2), yd1)
-        	yd2 = k2 * (dt/2) + yp
-        
-        	k3 = explicit_diff(x+(dt/2), yd2)
-        	yd3 = k3 * dt + yp
-        
-        	k4 = explicit_diff(x+dt, yd3)
-        	ktot = (k1 + 2*k2 + 2*k3 + k4)/6
-        
-        	new_y = yp + dt * ktot
-        	y_points.append(new_y)
-        	x_points.append(x+dt)
-	return (x_points, y_points)
-	
+def numerical_integrate(explicit_diff, x_0, x1_0, dt, steps=1):
+	return runge_kutta(explicit_diff, x_0, x1_0, dt, steps=steps)
+
+def runge_kutta(explicit_diff, x_0, x1_0, dt, steps):
+    rk = np.zeros((steps + 1,2))
+    rk[0][0] = x_0
+    rk[0][1] = x1_0
+
+    for i in range(steps):
+        k1 = np.array([rk[i-1][1], explicit_diff(rk[i-1][0])])
+        # Halber Euler-Schritt
+        v_tmp = rk[i-1] + k1*dt/2
+        k2 = np.array([v_tmp[1], explicit_diff(v_tmp[0])])
+
+        # Halber Euler-Schritt mit der neuen Steigung
+        v_tmp = rk[i-1] + k2*dt/2
+        k3 = np.array([v_tmp[1], explicit_diff(v_tmp[0])])
+
+        # ganzer Euler-Schritt mit k3
+        v_tmp = rk[i-1] + k3*dt
+        k4 = np.array([v_tmp[1], explicit_diff(v_tmp[0])])
+
+        rk[i] = rk[i-1] + dt*(k1 + 2*k2 + 2*k3 + k4)/6
+
+    return rk[-1]
